963f69235afe64eeead6f602ac0e7cf3f79e952e
kaziabulhasib <hasibkazi420@gmail.com>
2024-09-26 00:02:37 +0530

    dark  mood change

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -49,7 +49,7 @@ const Skills = () => {
   return (
     <div id='skills'>
       <SectionTitle title='Skills' text='Explore My'></SectionTitle>
-      <div className='grid lg:grid-cols-3 grid-cols-2  lg:gap-16 gap-4 px-6 lg:w-2/3 w-full mx-auto     lg:my-24 my-6'>
+      <div className='grid lg:grid-cols-3 grid-cols-2  lg:gap-16 gap-6 px-6 lg:w-2/3 w-full mx-auto     lg:my-24 my-6'>
         {skillsData.map((skill, index) => (
           <div key={index}>
             <SkillsSubSec skill={skill.skill} img={skill.img}></SkillsSubSec>
