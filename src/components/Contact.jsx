f10d0410fd48dbfdc923e731b600984a460e2f21
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-16 12:18:08 +0530

    hide the scrollbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -105,24 +105,24 @@ const Contact = () => {
                   className='mt-6 flex flex-col flex-grow'>
                   <div className='flex-1'>
                     <label className='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
-                      Full Name
+                      Name
                     </label>
                     <input
                       type='text'
                       name='user_name'
-                      placeholder='John Doe'
+                      placeholder='Your Name'
                       className='block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
                     />
                   </div>
 
                   <div className='flex-1 mt-6'>
                     <label className='block mb-2 text-sm text-gray-600 dark:text-gray-200'>
-                      Email address
+                      Email 
                     </label>
                     <input
                       type='email'
                       name='user_email'
-                      placeholder='johndoe@example.com'
+                      placeholder='Your Email'
                       className='block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
                     />
                   </div>
--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({ title, text }) => {
       <p className='text-center lg:w-1/3 w-full  mx-auto text-gray-500 dark:text-slate-300 text-xl my-4 italic '>
         {text}
       </p>
-      <h1 className='text-center capitalize  pb-6 mx-auto lg:w-1/2 w-full text-black dark:text-white lg:text-5xl text-4xl font-bold'>
+      <h1 className='text-center capitalize  pb-6 mx-auto lg:w-1/2 w-full text-black dark:text-white lg:text-3xl text-2xl font-bold'>
         {title}
       </h1>
     </div>
